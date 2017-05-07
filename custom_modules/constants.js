b2d15d8fcd1223c455e3f5672c35a9ed3fb3206d
Mackenzie Clark <mackenzieclark@protonmail.ch>
2017-05-07 13:39:00 -0700

    Add headers module for maintaining OAuth token state and other conditional headers

--- custom_modules/constants.js
+++ custom_modules/constants.js
@@ -8,5 +8,4 @@ module.exports = {
     get_visitors: 'http://www.okcupid.com/visitors?okc_api=1',
     quickmatch: 'http://www.okcupid.com/quickmatch?okc_api=1'
     },
-    HEADERS: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'}
 }
