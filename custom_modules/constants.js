bb3328995b8c5b067652fe1fb2eec7dd002a36a1
Mackenzie Clark <mackenzieclark@protonmail.ch>
2017-05-07 13:40:00 -0700

    Add like and unlike methods, document methods in README

--- custom_modules/constants.js
+++ custom_modules/constants.js
@@ -6,6 +6,10 @@ module.exports = {
     user_profile: 'http://www.okcupid.com/profile/{username}?okc_api=1',
     user_questions: 'http://www.okcupid.com/profile/{username}/questions?okc_api=1&low={low}',
     get_visitors: 'http://www.okcupid.com/visitors?okc_api=1',
-    quickmatch: 'http://www.okcupid.com/quickmatch?okc_api=1'
-    },
+    quickmatch: 'http://www.okcupid.com/quickmatch?okc_api=1',
+    
+    // OAuth API
+    like: 'https://www.okcupid.com/1/apitun/profile/{userid}/like',
+    unlike: 'https://www.okcupid.com/1/apitun/profile/{userid}/unlike',
+	}
 }
