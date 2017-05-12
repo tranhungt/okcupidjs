2660038375ee852221f5b60b7d96f6db5a61206f
Mackenzie Clark <mackenzieclark@protonmail.ch>
2017-05-11 21:38:10 -0700

    Add get_messages, get_thread, and send_message url strings

--- custom_modules/constants.js
+++ custom_modules/constants.js
@@ -7,9 +7,12 @@ module.exports = {
     user_questions: 'http://www.okcupid.com/profile/{username}/questions?okc_api=1&low={low}',
     get_visitors: 'http://www.okcupid.com/visitors?okc_api=1',
     quickmatch: 'http://www.okcupid.com/quickmatch?okc_api=1',
+    get_messages: 'https://www.okcupid.com/messages?okc_api=1',
+    get_thread: 'https://www.okcupid.com/messages?okc_api=1&readmsg=true&threadid={thread_id}',
     
     // OAuth API
     like: 'https://www.okcupid.com/1/apitun/profile/{userid}/like',
     unlike: 'https://www.okcupid.com/1/apitun/profile/{userid}/unlike',
+    send_message: 'https://www.okcupid.com/1/apitun/messages/send',
 	}
 }
