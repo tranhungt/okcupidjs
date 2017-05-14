ca86b363fd276f5cd4d911e43ae58ca786608c26
Mackenzie Clark <mackenzieclark@protonmail.ch>
2017-05-14 10:10:41 -0700

    Add search url constant

--- custom_modules/constants.js
+++ custom_modules/constants.js
@@ -14,5 +14,6 @@ module.exports = {
     like: 'https://www.okcupid.com/1/apitun/profile/{userid}/like',
     unlike: 'https://www.okcupid.com/1/apitun/profile/{userid}/unlike',
     send_message: 'https://www.okcupid.com/1/apitun/messages/send',
+    search: 'https://www.okcupid.com/1/apitun/match/search',
   }
 }
