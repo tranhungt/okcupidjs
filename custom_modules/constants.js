7ea41c612ae8e7724a363974117393ac621d6560
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2018-08-13 22:07:07 -0700

    correct get all messages and get message thread

--- custom_modules/constants.js
+++ custom_modules/constants.js
@@ -7,10 +7,11 @@ module.exports = {
     user_questions: 'http://www.okcupid.com/profile/{username}/questions?okc_api=1&low={low}',
     get_visitors: 'http://www.okcupid.com/visitors?okc_api=1',
     quickmatch: 'http://www.okcupid.com/quickmatch?okc_api=1',
-    get_messages: 'https://www.okcupid.com/messages?okc_api=1',
-    get_thread: 'https://www.okcupid.com/messages?okc_api=1&readmsg=true&threadid={thread_id}',
 
     // OAuth API
+    get_messages: 'https://www.okcupid.com/1/apitun/messages',
+    get_thread: 'https://www.okcupid.com/1/apitun/messages/conversations/{thread_id}',
+
     like: 'https://www.okcupid.com/1/apitun/profile/{userid}/like',
     unlike: 'https://www.okcupid.com/1/apitun/profile/{userid}/unlike',
     send_message: 'https://www.okcupid.com/1/apitun/messages/send',
