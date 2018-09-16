3268f6b2fca0504556f58a23da55c6897accf6a8
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2018-09-16 09:23:42 -0700

    fix search example

--- examples/search/index.js
+++ examples/search/index.js
@@ -6,16 +6,16 @@ var okc = Promise.promisifyAll(new OKCupid());
 const baseQuery = {i_want: "men", they_want: "women" , radius: 25 };
 
 const search = async () => {
-  if (!process.env.USERNAME) {
+  if (!process.env.OKC_USERNAME) {
     console.log('Env file does not exist. Rename .env.example to .env and set the username and password variables.');
     return;
   }
-  if (process.env.USERNAME === 'username') {
+  if (process.env.OKC_USERNAME === 'username') {
     console.log("Set the username and password in the env file.");
     return;
   }
   try {
-    await okc.loginAsync(process.env.USERNAME, process.env.PASSWORD);
+    await okc.loginAsync(process.env.OKC_USERNAME, process.env.OKC_PASSWORD);
     console.log('login done');
   } catch (e) {
     console.log('Failed to login. Check your username and password in the env file.');
@@ -31,14 +31,15 @@ const search = async () => {
         console.log(user.username);
       });
       var hash = body.paging.cursors.after;
-      // check termination condtion, otherwise recurse
+      // check termination condition, otherwise recurse
       if (hash != null && count <= 100) {
+        baseQuery.after = hash;
         count = count + body.data.length;
         search();
       }
     });
   };
-  s
+
   search();
 };
 
